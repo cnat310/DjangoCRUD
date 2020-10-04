8d8f49cf93c44a4c6963ff9ef3d0b75c0f08220c
cnat310 <cnat310@gmail.com>
2020-10-04 11:22:17 -0500

    added host

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '%fjfw75idwg1+fi_kh2t9yzbb#clzfbtb)+c4oa(r6vn7$zi%c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'versumevents.herokuapp.com',
+]
 
 
 # Application definition
